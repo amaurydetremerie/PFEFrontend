0230a471b443ee3bb28f574e1029868740e39414
Amaury De Tremerie <amaury.detremerie@student.vinci.be>
2021-12-16 01:57:46 +0100

    merged

--- src/app/single-offer/single-offer.component.ts
+++ src/app/single-offer/single-offer.component.ts
@@ -12,43 +12,29 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class SingleOfferComponent implements OnInit {
   // @ts-ignore
   offers: Offers;
-  isAdmin:boolean = false;
   id;
 
   displayedColumns = ['title', 'description', 'place', 'sellerEMail'];
   constructor(
-    private _Activatedroute: ActivatedRoute,
-    private _router: Router,
-    private _offerService: OffersService
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private offerService: OffersService
   ) {}
 
   ngOnInit(): void {
-    this.id = this._Activatedroute.snapshot.params['id'];
-    this.getOffersById(this.id);
-    this.isAdmin=localStorage.getItem("isAdmin")==="true";
+    const id = this.activatedRoute.snapshot.params['id'];
+    console.log(id);
+    this.getOffersById(id);
   }
 
   getOffersById(id: string): void {
-    let idToGet = parseInt(id);
-    this._offerService.getById(idToGet).subscribe((offers: Offers) => {
+    // tslint:disable-next-line:radix
+    this.offerService.getById(parseInt(id)).subscribe((offers: Offers) => {
       this.offers = offers;
     });
   }
 
-  
-  resetSignalements(){
-    this._offerService.resetSignalements(this.id).subscribe(result=>this._router.navigateByUrl("/admin/signalements"));
-  }
-
-  deleteOffer(): void {
-    this._offerService.deleteOffer(this.id).subscribe(result=>this._router.navigateByUrl("/admin/signalements"));
-  }
-
-  signalerOffer(){
-    this._offerService.signalerOffer(this.id).subscribe(result=>alert("Annonce signale"));
-  }
-
   onBack(): void {
-    this._router.navigate(['offers']);
+    this.router.navigate(['offers']);
   }
 }
